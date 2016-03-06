3a6a1bc66786ef0134086f9a141ba7799cfe1210
Jeff Ramos <jeffs.ramos@gmail.com>
2016-03-06 20:36:46 +0100

    first commit

---
+++ Project/Assets/Scripts/Game/Controller/GameController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class GameController : MonoBehaviour {
+    public RectTransform actionArea;
+    public GameObject actionButtonPrefab;
+    public float gameTime = 0;
+    private List<ActionButtonView> buttonWave;
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameTime += Time.deltaTime;
+
+        if(Mathf.Floor(gameTime) % 3 == 1)
+        {
+            addAction();
+            gameTime = 0;
+            //print(gameTime);
+        }
+
+    }
+
+    private void addAction()
+    {
+        Vector3 tempV3 = new Vector3(UnityEngine.Random.Range(actionArea.rect.min.x, actionArea.rect.max.x),
+                        UnityEngine.Random.Range(actionArea.rect.min.y, actionArea.rect.max.y),
+                        0);
+        GameObject tempObject = (GameObject)Instantiate(actionButtonPrefab, tempV3, Quaternion.identity);
+        tempObject.transform.SetParent(actionArea.transform, false);
+    }
+}
---
+++ Project/Assets/Scripts/Game/View/ActionButtonView.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+using System;
+using UnityEngine.UI;
+
+public class ActionButtonView : MonoBehaviour {
+    public GameObject newState;
+    public GameObject feedbackState;
+    public RectTransform innerContent;
+    public RectTransform outerContent;
+    public Text text;
+    public Button button;
+    public Vector2 scale;
+    private float currentScale = 5f;
+    public float maxScale = 5f;
+    public float minScale = 0.5f;
+    private float timeDecress = 5f;
+    public float maxTime = 5f;
+    public bool finishing;
+    public Animator feedbackAnimator;
+    // Use this for initialization
+    void Start () {
+
+        finishing = false;
+        updateScale();
+        Build(2);
+
+        timeDecress = maxTime;
+
+        button.onClick.AddListener(()=> {
+            ClickCallback();
+        });
+
+        newState.SetActive(true);
+        feedbackState.SetActive(false);
+    }
+
+    private void ClickCallback()
+    {
+        print("CLICK");
+        finish();        
+    }
+
+    public void Build(int level)
+    {
+        text.text = level.ToString();
+    }
+    private void updateScale()
+    {
+        Vector2 tempScale = outerContent.localScale;
+        tempScale.x = currentScale;
+        tempScale.y = currentScale;
+        outerContent.localScale = tempScale;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (finishing)
+        {
+            if (!feedbackAnimator.GetCurrentAnimatorStateInfo(0).IsName("ActionFeedbackAnimation"))
+            {
+                feedbackAnimator.Stop();
+                Destroy(this.gameObject);
+                // Do something.
+            }
+            return;
+        }
+        if (currentScale <= minScale)
+        {
+            //updateScale();
+            finish();
+        }
+        else {
+            timeDecress -= Time.deltaTime;
+            
+            currentScale = (maxScale * (timeDecress / maxTime));
+            updateScale();
+            //print(maxScale +" - "+ maxTime + " - " + timeDecress + " - " + currentScale);
+        }
+
+    }
+
+    private void finish()
+    {
+        finishing = true;
+
+        newState.SetActive(false);
+        feedbackState.SetActive(true);
+        //StartCoroutine(feedbackState.animation.PlayWithOptions(
+        //         animToPlay,
+        //         () =>
+        //         {
+        //             Debug.Log("ANIMATION FINISHED, LOADING SCENE X");
+        //             Application.LoadLevel("SceneX");
+        //         }
+        //         )
+        //     );
+
+        
+        //print(currentScale);
+    }
+}
